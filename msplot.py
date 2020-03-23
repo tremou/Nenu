7d8f0428cd4b1f3c18c175ae201c9c14ab47b89e
Evangelia (Lilia) Tremou <tremou@users.noreply.github.com>
2020-03-23 15:14:20 +0100

    added DATA or CORRECTED_DATA column option

--- msplot.py
+++ msplot.py
@@ -17,12 +17,13 @@ parser = argparse.ArgumentParser(description='Quick plots of MSs', epilog="Outpu
 parser.add_argument("MS_file", help='input MeasurmentSet file')
 parser.add_argument('-corr','--corr',dest='corr',help='Correlation index to plot. 0 or 1 or 2 or 3, default = 0 (0=XX, 1=XY, 2=YX, 3=YY)',default=0)
 parser.add_argument('-ant','--ant',dest='ant',help='Plot only this antenna, or comma-separated list of antennas',default=[-1])
+parser.add_argument('-datacol','--datacol',dest='datacol',help='Plot DATA (data) column or CORRECTED_DATA (corrdata) column',default='data')
 
 args = parser.parse_args()
 parser.print_help()
 corr = int(args.corr)
 ant = args.ant
-
+datacol = str(args.datacol)
 
 # Disable
 def blockPrint():
@@ -87,7 +88,11 @@ phase = numpy.angle(data[:,:,corr])#rads
 amp = numpy.abs(data[:,:,corr])
 real = numpy.real(data[:,:,corr])
 imag = numpy.imag(data[:,:,corr])
-
+corrdata = maintab.getcol('CORRECTED_DATA')
+phase_corr = numpy.angle(corrdata[:,:,corr])#rads
+amp_corr = numpy.abs(corrdata[:,:,corr])
+real_corr = numpy.real(corrdata[:,:,corr])
+imag_corr = numpy.imag(corrdata[:,:,corr])
 times = maintab.getcol('TIME')
 t0 = times[0]
 t1 = times[-1]
@@ -115,18 +120,35 @@ else:
 print(plt.rcParams['agg.path.chunksize'])
 plt.rcParams['agg.path.chunksize'] = 10000
 fig, axs = plt.subplots(2, 2, figsize=(15,9))
-fig.suptitle(myms,fontsize = 10)
-axs[0, 0].plot(uu, vv, ',', markersize =15)
-axs[0, 0].set(xlabel='U', ylabel='V')
-axs[0, 1].plot(real, imag, ',', markersize =5, color='tab:orange')#, 'tab:orange')
-axs[0, 1].ticklabel_format(useOffset=False, style='plain')
-axs[0, 1].set(xlabel='Real', ylabel='Imaginary')
-axs[0, 1].ticklabel_format(useOffset=False, style='plain')
-axs[1, 0].plot(uvdist, amp, ',', markersize =3, color='tab:green')
-axs[1, 0].set(xlabel='UV distance', ylabel='Amplitude')
-axs[1, 0].ticklabel_format(useOffset=False, style='plain')
-axs[1, 1].plot(times/86400, amp, ',', markersize =3, color='tab:red')
-axs[1, 1].set(xlabel='Time (mjd)', ylabel='Amplitude')
-axs[1, 1].ticklabel_format(useOffset=False, style='plain')
-fig.savefig(myms+'_plot.png',bbox_inches='tight')
+fig.suptitle(myms+'_'+datacol,fontsize = 10)
+
+if datacol=='corrdata':
+
+    axs[0, 0].plot(uu, vv, ',', markersize =15, alpha=0.3)
+    axs[0, 0].set(xlabel='U', ylabel='V')
+    axs[0, 1].plot(real_corr, imag_corr, ',', markersize =5, color='tab:orange', alpha=0.3)#, 'tab:orange')
+    axs[0, 1].ticklabel_format(useOffset=False, style='plain')
+    axs[0, 1].set(xlabel='Real', ylabel='Imaginary')
+    axs[0, 1].ticklabel_format(useOffset=False, style='plain')
+    axs[1, 0].plot(uvdist, amp_corr, ',', markersize =3, color='tab:green', alpha=0.3)
+    axs[1, 0].set(xlabel='UV distance', ylabel='Amplitude')
+    axs[1, 0].ticklabel_format(useOffset=False, style='plain')
+    axs[1, 1].plot(times/86400, amp_corr, ',', markersize =3, color='tab:red', alpha=0.3)
+    axs[1, 1].set(xlabel='Time (mjd)', ylabel='Amplitude')
+    axs[1, 1].ticklabel_format(useOffset=False, style='plain')
+
+else:
+    axs[0, 0].plot(uu, vv, ',', markersize =15, alpha=0.3)
+    axs[0, 0].set(xlabel='U', ylabel='V')
+    axs[0, 1].plot(real, imag, ',', markersize =5, color='tab:orange', alpha=0.3)#, 'tab:orange')
+    axs[0, 1].ticklabel_format(useOffset=False, style='plain')
+    axs[0, 1].set(xlabel='Real', ylabel='Imaginary')
+    axs[0, 1].ticklabel_format(useOffset=False, style='plain')
+    axs[1, 0].plot(uvdist, amp, ',', markersize =3, color='tab:green', alpha=0.3)
+    axs[1, 0].set(xlabel='UV distance', ylabel='Amplitude')
+    axs[1, 0].ticklabel_format(useOffset=False, style='plain')
+    axs[1, 1].plot(times/86400, amp, ',', markersize =3, color='tab:red', alpha=0.3)
+    axs[1, 1].set(xlabel='Time (mjd)', ylabel='Amplitude')
+    axs[1, 1].ticklabel_format(useOffset=False, style='plain')
+fig.savefig(myms+'_'+datacol+'_plot.png',bbox_inches='tight')
 plt.show()
