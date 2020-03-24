63f85b94fef3646e6926a0b67ef7af3f059c1064
Evangelia (Lilia) Tremou <tremou@users.noreply.github.com>
2020-03-24 10:46:19 +0000

    Add files via upload

---
+++ subtraction.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import os
+import glob
+import numpy
+
+mslist = glob.glob('*.ms')
+
+
+#available_steps=[source subtraction]
+steps= [1]
+
+if 1 in steps: 
+	for myms in mslist:
+		syscall= 'NDPPP ' 
+		syscall+= 'msin='+myms+' '
+		syscall+= 'msin.datacolumn=CORRECTED_DATA '
+		syscall+= 'msout='+myms+'_sub ' 
+		syscall+= 'steps=[subtractcasA, applycal] '
+		syscall+= 'subtractcasA.type=predict '
+ 		syscall+= 'subtractcasA.operation=subtract '
+		syscall+= 'subtractcasA.sourcedb=/data/etremou/nenufar/skymodel/CasApoint.sourcedb '
+		syscall+= 'subtractcasA.sources=[CasAPoint] '
+		syscall+= 'subtractcasA.applycal.parmdb='+myms+'/instrument '
+		syscall+= 'applycal.parmdb='+myms+'/instrument '
+		print (syscall) 
+		os.system(syscall)
+
